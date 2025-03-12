66a4d5e57b6ceef58f22768d069cf5940754cee8
Arav Menon <aravmenon.ak@gmail.com>
2025-03-13 03:55:28 +0530

    added content CRUD endpoints

--- dist/Routes/createContent.js
+++ dist/Routes/createContent.js
@@ -17,3 +17,54 @@ contentRouter.post("/create-content", user_1.userMiddlewares, async (req, res) =
         createContent
     });
 });
+contentRouter.get("/content", user_1.userMiddlewares, async (req, res) => {
+    const contents = await db_1.ContentModel.find();
+    if (!contents) {
+        res.status(404).json({
+            message: "You're not logIn"
+        });
+    }
+    res.json({
+        message: "Data",
+        contents
+    });
+});
+contentRouter.put("/update-content", user_1.userMiddlewares, async (req, res) => {
+    try {
+        const { link, title } = req.body;
+        const findUpdate = await db_1.ContentModel.findOneAndUpdate({
+            link,
+            title
+        });
+        if (!findUpdate) {
+            res.status(404).json({
+                message: "Content not found"
+            });
+        }
+        res.status(200).json({
+            message: "Content updated",
+            findUpdate
+        });
+    }
+    catch (e) {
+        res.json({
+            e: e
+        });
+    }
+});
+contentRouter.delete("/delete-content", user_1.userMiddlewares, async (req, res) => {
+    const { title, link } = req.body;
+    const deleteContent = await db_1.ContentModel.findOneAndDelete({
+        title,
+        link
+    });
+    if (!deleteContent) {
+        res.status(404).json({
+            message: "Data not found"
+        });
+    }
+    res.status(204).json({
+        message: "Delted succesfully",
+        deleteContent
+    });
+});
